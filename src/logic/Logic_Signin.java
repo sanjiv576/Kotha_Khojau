f41b5e5af3bec444a8a8cd3b2797eab08e7f5a5c
sanjiv576 <83968516+sanjiv576@users.noreply.github.com>
2022-01-20 22:20:00 +0545

    checking whether provided username and password are in the database or not

--- src/logic/Logic_Signin.java
+++ src/logic/Logic_Signin.java
@@ -1,6 +1,10 @@
 package logic;
 
+import controller.LoginDataController;
+import model.LoginData;
+
 import javax.swing.*;
+import java.sql.ResultSet;
 
 public class Logic_Signin {
     private String username;
@@ -19,8 +23,32 @@ public class Logic_Signin {
                     "Error", JOptionPane.ERROR_MESSAGE);
         }
         else {
-            JOptionPane.showMessageDialog(null, "Login successfully",
-                    "Logged In ", JOptionPane.YES_OPTION);
+
+            LoginData loginData = new LoginData(username, password);
+
+            LoginDataController loginDataController = new LoginDataController();
+
+            ResultSet result;
+
+            try{
+                result = loginDataController.checkValidation(username, password);
+
+                if (result.next()){
+                    JOptionPane.showMessageDialog(null, "Provided username and password are in the database.", "Check in database", JOptionPane.INFORMATION_MESSAGE);
+                }
+                else {
+                    JOptionPane.showMessageDialog(null, "Provided username and password are not in the database.", "Check in database", JOptionPane.WARNING_MESSAGE);
+                }
+
+            }
+            catch(Exception exp){
+                exp.getStackTrace();
+                System.out.println(exp.getMessage());
+            }
+
+            System.out.println("After login Button");
+//            JOptionPane.showMessageDialog(null, "Login successfully",
+//                    "Logged In ", JOptionPane.YES_OPTION);
         }
         //return 0;
     }
