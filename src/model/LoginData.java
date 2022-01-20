c950160513ece5ef2491285fa8c06a42a99fd8ba
sanjiv576 <83968516+sanjiv576@users.noreply.github.com>
2022-01-20 22:19:38 +0545

    getter and setter methods are included.

---
+++ src/model/LoginData.java
@@ -0,0 +1,34 @@
+package model;
+
+public class LoginData {
+    // values come form Login Windows;
+    //private int UserID;
+    private String username;
+    private String password;
+
+    public LoginData(String username, String password){
+
+        this.username = username;
+        this.password = password;
+    }
+
+    // getter method for username
+    public String getUsername(){
+        return username;
+    }
+
+    // setter method for username
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    // getter method for password
+    public String getPassword(){
+        return password;
+    }
+
+    // setter method for password
+    public void setPassword(String password){
+        this.password = password;
+    }
+}
