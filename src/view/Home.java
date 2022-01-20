258804e968ca892d7ee48b060df156c778935ba0
uti <manisha54>
2022-01-20 10:15:13 +0545

    home java class added and connected to setting

---
+++ src/view/Home.java
@@ -0,0 +1,157 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Objects;
+
+public class Home extends JFrame implements ActionListener {
+
+    // view.Images Labeling
+    JLabel vertical, horizontal, titleImg , projectLogoImg;
+
+    JButton homeIcon, profileIcon, settingIcon,driverIcon, logoutIcon;
+
+
+    public Home() {
+
+        setTitle("Home Page");
+        setBounds(100, 80, 1280, 745);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setBackground(Color.darkGray);
+        setLayout(null);
+        setResizable(false);
+
+
+        // invoking user-defined method to insert and manage icons for dashboard
+        insertButtonIcons();
+
+
+        // your code will be here
+
+
+
+
+
+
+
+        logoutIcon.addActionListener(this);
+        settingIcon.addActionListener(this);
+        homeIcon.addActionListener(this);
+        profileIcon.addActionListener(this);
+
+    }
+
+    public static void main(String[] args) {
+        new Home().setVisible(true);
+    }
+
+    //  event handling
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if (e.getSource().equals(logoutIcon)) {
+            int choice = JOptionPane.showConfirmDialog(null, "Do you want to log out ?",
+                    "Log out", JOptionPane.YES_NO_CANCEL_OPTION);
+            if (choice == JOptionPane.YES_OPTION) {
+                System.exit(0);
+            }
+        }
+
+        if (e.getSource().equals(settingIcon)){
+            dispose();
+            new New_updateprofile().setVisible(true);
+        }
+
+        if (e.getSource().equals(profileIcon)){
+            dispose();
+            new Renter_userprofile().setVisible(true);
+        }
+    }
+
+    //  -------------- DON'T CHANGE THIS ----------------
+    // this method inserts left button icons
+    public void insertButtonIcons(){
+
+        projectLogoImg = new JLabel();
+        projectLogoImg.setIcon(new ImageIcon((Objects.requireNonNull(getClass().getResource("Images/upperLogo174x137.png")))));
+        projectLogoImg.setBounds(0, -15, 165, 148);
+        add(projectLogoImg);
+
+        homeIcon = new JButton();
+        homeIcon.setIcon(new ImageIcon((Objects.requireNonNull(getClass().getResource("Images/greenhome_100x93.png")))));
+        homeIcon.setBounds(1, 150, 100, 100);
+        homeIcon.setBackground(Color.decode("#9E9B9B"));
+        homeIcon.setOpaque(true);
+        homeIcon.setBorderPainted(false);
+        homeIcon.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        add(homeIcon);
+
+
+        profileIcon = new JButton();
+        profileIcon.setIcon(new ImageIcon((Objects.requireNonNull(getClass().getResource("Images/profileImg_78x64.png")))));
+        profileIcon.setBounds(1, 250, 100, 100);
+        profileIcon.setBackground(Color.decode("#9E9B9B"));
+        profileIcon.setOpaque(true);
+        profileIcon.setBorderPainted(false);
+        profileIcon.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        add(profileIcon);
+
+
+        settingIcon = new JButton();
+        settingIcon.setIcon(new ImageIcon((Objects.requireNonNull(getClass().getResource("Images/settingIcon64x61.png")))));
+        settingIcon.setBounds(1, 353, 100, 100);
+        settingIcon.setBackground(Color.decode("#9E9B9B"));
+        settingIcon.setOpaque(true);
+        settingIcon.setBorderPainted(false);
+        settingIcon.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        add(settingIcon);
+
+        driverIcon= new JButton();
+        driverIcon.setIcon(new ImageIcon((Objects.requireNonNull(getClass().getResource("Images/driver64X64.png")))));
+        driverIcon.setBounds(1, 480, 100, 100);
+        driverIcon.setBackground(Color.decode("#9E9B9B"));
+        driverIcon.setOpaque(true);
+        driverIcon.setBorderPainted(false);
+        driverIcon.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        add(driverIcon);
+
+        logoutIcon = new JButton();
+        logoutIcon.setIcon(new ImageIcon((Objects.requireNonNull(getClass().getResource("Images/logout_64x64.png")))));
+        logoutIcon.setBounds(15, 605, 64, 64);
+        logoutIcon.setBackground(Color.decode("#9E9B9B"));
+        logoutIcon.setOpaque(true);
+        logoutIcon.setBorderPainted(false);
+        logoutIcon.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        add(logoutIcon);
+
+        titleImg = new JLabel();
+        titleImg.setIcon(new ImageIcon((Objects.requireNonNull(getClass().getResource("Images/projectTitleImg_222x58.png")))));
+        titleImg.setBounds(135, 22, 222, 58);
+        add(titleImg);
+
+        // top horizontal layer that includes project title
+        horizontal = new JLabel();
+        horizontal.setIcon(new ImageIcon((Objects.requireNonNull(getClass().getResource("Images/horizontal_img_1320x106.png")))));
+        horizontal.setBounds(-40, 0, 1320, 106);
+        //horizontal.setBackground(Color.decode("#9E9B9B"));
+        horizontal.setOpaque(true);
+        add(horizontal);
+
+        // left  vertical layer that includes project logo and buttons
+        vertical = new JLabel();
+        vertical.setIcon(new ImageIcon((Objects.requireNonNull(getClass().getResource("Images/vertical_line_104x720.png")))));
+        vertical.setBounds(0, 0, 104, 720);
+        vertical.setBackground(Color.getColor("9E9B9B"));
+        vertical.setOpaque(true);
+        add(vertical);
+    }
+
+}
+
+
+
+
+
+
