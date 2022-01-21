13364c1655923fa5462b80d169600d5c1db75a5d
Srijan0130 <gurungmotey9@gmail.com>
2022-01-21 14:28:32 +0545

    test commit

--- src/view/test.java
+++ src/view/test.java
@@ -1,4 +1,29 @@
-//package view;
+package view;//package view;
 //
-//public class test {
-//}
+import java.util.Scanner;
+public class test {
+        public static void main(String [ ] args){
+                Scanner sc= new Scanner(System.in);
+                int first_num;
+                int second_num;
+
+                System.out.println("Enter First Number:");
+                first_num= sc.nextInt();
+
+                System.out.println("Enter Second Number:");
+                second_num= sc.nextInt();
+
+                if (first_num>second_num){
+                        System.out.println(first_num+" Is greater!!!");}
+
+                else if (second_num>first_num){
+                        System.out.println(second_num+" Is greater!!!");}
+
+                else {
+                        System.out.println("THE BOTH NUMBER EQUAL");
+                }
+
+        }
+
+}
+
