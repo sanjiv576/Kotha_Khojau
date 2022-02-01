559f62237e34aeb579122b9ace8a0842694d8b19
sanjiv576 <83968516+sanjiv576@users.noreply.github.com>
2022-02-01 16:45:08 +0545

    saving username and password of user to access from any class

---
+++ src/logic/SaveData.java
@@ -0,0 +1,10 @@
+package logic;
+// saves data that which is accessible
+public class SaveData {
+    static public String myUsername ;
+    static public String myPassword;
+    public SaveData(String myUsername, String myPassword){
+        SaveData.myUsername = myUsername;
+        SaveData.myPassword = myPassword;
+    }
+}
